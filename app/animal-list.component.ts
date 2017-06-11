4107730faccde2917430b619b32535e7cc1a2a70
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-06-11 13:36:02 -0700

    remove animal

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -5,41 +5,50 @@ import { Animal } from './animal.model';
   selector: 'animal-list',
   template: `
 
-  <h3>See all animals currently at the Zoo</h3>
-  <ul>
-    <li *ngFor="let currentAnimal of childAnimalList">
-    <img class ="profile-pic" src="{{currentAnimal.image}}">
-    {{currentAnimal.name}},
-    {{currentAnimal.species}}
-    {{currentAnimal.age}},
-    {{currentAnimal.diet}},
-    {{currentAnimal.location}},
-    {{currentAnimal.caretakers}},
-    {{currentAnimal.sex}},
-    {{currentAnimal.likes}},
-    {{currentAnimal.dislikes}}
-    <button (click)="editAnimalButtonClicked(currentAnimal)">Edit Info</button></li>
-  </ul>
+<div class="row">
+  <div class="col-md-3" *ngFor="let currentAnimal of childAnimalList">
+  <div class="animal-table">
+    <table>
+    <tr>
+       <th><h3>{{currentAnimal.name}}</h3></th>
+    </tr>
+     <tr>
+        <th><img class ="profile-pic" src="{{currentAnimal.image}}"></th>
+     </tr>
+     <tr>
+        <th><h4>{{currentAnimal.species}}</h4></th>
+     </tr>
+     <tr>
+        <td> age {{currentAnimal.age}}</td>
+     </tr>
+     <tr>
+        <td>{{currentAnimal.sex}}</td>
+     </tr>
+     <tr>
+        <td>{{currentAnimal.diet}}</td>
+     </tr>
+     <tr>
+        <td>{{currentAnimal.caretakers}} caretakers</td>
+     </tr>
+     <tr>
+        <td>@ {{currentAnimal.location}}</td>
+     </tr>
+     <tr>
+        <td>{{currentAnimal.likes}}</td>
+     </tr>
+     <tr>
+        <td>{{currentAnimal.dislikes}}</td>
+     </tr>
+     <br>
+     <tr>
+      <td><button (click)="editAnimalButtonClicked(currentAnimal)">Edit Info</button></td>
+    </tr>
+    <tr>
+     <td><button (click)="deleteAnimalButtonClicked(currentAnimal)">Remove Animal</button></td>
+   </tr>
+  </table>
   <br>
-  <h3>View animals by age:</h3>
-  <select (change)="onChange($event.target.value)">
-    <option value="youngAnimals" selected="selected">View Young Animals</option>
-    <option value="matureAnimals">View Mature Animals</option>
-  </select>
-  <ul>
-    <li *ngFor="let currentAnimal of childAnimalList | animalAge:filterByAnimalAge">
-    <img class ="profile-pic" src="{{currentAnimal.image}}">
-    {{currentAnimal.name}},
-    {{currentAnimal.species}}
-    {{currentAnimal.age}},
-    {{currentAnimal.diet}},
-    {{currentAnimal.location}},
-    {{currentAnimal.caretakers}},
-    {{currentAnimal.sex}},
-    {{currentAnimal.likes}},
-    {{currentAnimal.dislikes}}
-  </ul>
-  <br>
+  </div>
   `
 })
 
@@ -47,17 +56,38 @@ export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
   @Output() clickAddSender = new EventEmitter();
-  filterByAnimalAge: string = "youngAnimals"
+  @Output() clickDeleteSender = new EventEmitter();
+  filterByAnimalAge: string = "allAnimals"
 
   onChange(dropdownOptionSelected) {
     this.filterByAnimalAge = dropdownOptionSelected;
   }
 
-  editAnimalButtonClicked(animalToEdit: Animal) {
+  editAnimalButtonClicked(childAnimalList: Animal[], animalToEdit: Animal) {
     this.clickSender.emit(animalToEdit);
+  }
+
+  deleteAnimalButtonClicked(animalToRemove: Animal) {
+    this.clickDeleteSender.emit(animalToRemove);
   }
 
   addAnimalButtonClicked() {
     this.clickAddSender.emit();
   }
+
+
 }
+
+
+// <h3>View animals by age:</h3>
+// <select (change)="onChange($event.target.value)">
+//   <option value="allAnimals">View All Animals</option>
+//   <option value="youngAnimals">View Young Animals</option>
+//   <option value="matureAnimals">View Mature Animals</option>
+// </select>
+// <h3>See all animals currently at the Zoo</h3>
+// <ul>
+
+// <div class="row">
+//   <div class="col-md-3" *ngFor="let currentAnimal of childAnimalList | animalAge:filterByAnimalAge">
+//   <div class="animal-table">
--- app/app.component.ts
+++ app/app.component.ts
@@ -9,9 +9,9 @@ import { Animal } from './animal.model';
         <h1>Zoo of the Wild</h1>
       </div>
       <div class="box">
-        <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
-        <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()"></edit-animal>
         <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
+        <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)" (clickDeleteSender)="deleteAnimal($event)"></animal-list>
+        <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()"></edit-animal>
       </div>
     </div>
     <br>
@@ -24,6 +24,7 @@ export class AppComponent {
     new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based', 'http://felid-tag.org/wp-content/uploads/2014/03/ocelot2-Dan-Bodenstein1.jpg'),
   ];
   selectedAnimal: Animal = null;
+  chosenAnimal: Animal;
 
   editAnimal(clickedAnimal) {
     this.selectedAnimal = clickedAnimal;
@@ -35,5 +36,10 @@ export class AppComponent {
 
   addAnimal(newAnimal: Animal) {
     this.masterAnimalList.push(newAnimal);
+  }
+
+  deleteAnimal(thisAnimal: Animal) {
+    var index = this.masterAnimalList.indexOf(thisAnimal);
+    this.masterAnimalList.splice(index, 1);
   }
 }
